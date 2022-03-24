89b5574c87e0dcdbae68a6b656f49511a00c8cfe
Geert Fannes <geert.fannes@gmail.com>
2022-03-24 10:55:26 +0100

    wip

--- hyper_hop.py
+++ hyper_hop.py
@@ -4,3 +4,8 @@ import sublime_plugin
 class HyperHopCommand(sublime_plugin.TextCommand):
     def run(self, edit = None):
         print("Hello")
+        self.view.insert(edit, 0, "#import ole\n")
+        for region in self.view.sel():
+            str = self.view.substr(region)
+            str = str+str
+            self.view.replace(edit, region, str)
